1e5cab389bce1205de8521f633347ce8a0194a0b
Zhiqi Lu <zhiqilu8470@gmail.com>
2022-12-09 11:52:33 +0800

    fix: use docker compose to start service

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM eclipse-temurin:11
-ADD /opt/joi-news-1.0-SNAPSHOT.jar /app.jar
+ADD app.jar /app.jar
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,4 @@
-ansible server -m copy -a "src=../build/libs/joi-news-1.0-SNAPSHOT.jar dest=/opt/app.jar"
-ansible server -m copy -a "src=../Dockerfile dest=/opt/Dockerfile"
-ansible server -m shell -a "cd /opt docker \ build -t myapp:1.0"
+ansible server -m copy -a "src=../build/libs/joi-news-1.0-SNAPSHOT.jar dest=/opt/app.jar" --become
+ansible server -m copy -a "src=../Dockerfile dest=/opt/Dockerfile" --become
+ansible server -m shell -a "cd /opt \ docker build . -t myapp:1.0" --become
+ansible server -m shell -a "cd /opt \ docker-compose up --force-recreate" --become
