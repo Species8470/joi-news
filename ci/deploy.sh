a1ef75850842fbfa51ccc9cb6346211f2b47cdf4
Zhiqi Lu <zhiqilu8470@gmail.com>
2022-12-09 12:03:46 +0800

    fix: deploy script not executing last 2 steps

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,5 @@
-ansible server -m copy -a "src=../build/libs/joi-news-1.0-SNAPSHOT.jar dest=/opt/app.jar" --become
-ansible server -m copy -a "src=../Dockerfile dest=/opt/Dockerfile" --become
-ansible server -m shell -a "cd /opt \ docker build . -t myapp:1.0" --become
-ansible server -m shell -a "cd /opt \ docker-compose up --force-recreate" --become
+ansible server -m copy -a "src=../build/libs/joi-news-1.0-SNAPSHOT.jar dest=/home/ubuntu/app.jar" --become
+ansible server -m copy -a "src=../Dockerfile dest=/home/ubuntu/Dockerfile" --become
+ansible server -m copy -a "src=docker-compose.yml dest=/home/ubuntu/docker-compose.yml" --become
+ansible server -m shell -a "docker build . -t myapp:1.0" --become
+ansible server -m shell -a "docker-compose up --force-recreate" --become
